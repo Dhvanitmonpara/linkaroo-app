4a0ebd0ea0ef100118f4d2e25e85996276702d8f
Dhvanit <monparadhvanit@gmail.com>
2024-08-01 18:26:00 +0530

    added shadcn input component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        
+      </div>
     </>
   );
 }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./ui/input";
+
+const Header = () => {
+  return (
+    <nav>
+      <Input placeholder="Search something..." />
+    </nav>
+  );
+};
+
+export default Header;
