4ab91e699be3ff00d90dfe70c69dc55665a558fa
Dhvanit <monparadhvanit@gmail.com>
2024-08-03 19:10:05 +0530

    zustand setup

---
+++ client/src/store/listStore.js
@@ -0,0 +1,31 @@
+import { create } from "zustand"
+
+import { devtools, persist } from "zustand/middleware"
+
+const listStore = (set) => ({
+    list: [],
+    addListItem: (item) => {
+        set(state => ({ list: [...state.list, item] }))
+    },
+    removeListItem: (itemId) => {
+        set(state => ({ list: state.list.filter(item => item._id !== itemId) }))
+    },
+})
+
+const useListStore = create(
+    devtools(
+        persist(listStore, {
+            name: "lists"
+        })
+    )
+)
+
+export default useListStore;
+
+// usage
+
+// import useListStore from "./listStore"
+
+// const addListItem = useListStore((state) => state.addListItem)
+
+// const { addListItem, removeListItem } = useListStore(state => photo me dekhle)
