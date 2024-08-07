691e3faa2f33905994f0aff7f0d442057a03d810
Dhvanit <monparadhvanit@gmail.com>
2024-08-07 15:43:25 +0530

    major ui changes

--- client/src/components/ListCard.tsx
+++ client/src/components/ListCard.tsx
@@ -8,6 +8,7 @@ type ListCardProps = {
   tagname: string;
   description: string;
   title: string;
+  isBlackMode?: boolean;
   color: colorOptions;
   setIsModalOpen: (isOpen: boolean) => void;
 };
@@ -18,40 +19,70 @@ const imgArray = [
   "https://i.pinimg.com/236x/eb/09/69/eb096917cedb8fd3b3363d3dec531baa.jpg",
 ];
 
-const ListCard = ({ tagname, description, title, color, setIsModalOpen }: ListCardProps) => {
+const ListCard = ({
+  tagname,
+  description,
+  title,
+  isBlackMode = false,
+  color,
+  setIsModalOpen,
+}: ListCardProps) => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const openModal = (e:React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+  const openModal = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
     setIsModalOpen(true);
     e.stopPropagation();
     navigate(`/list?listid=${title}`, { replace: true });
     // TODO: open modal with content from props (add e.target.dataset.content into state)
-  }
+  };
   return (
     <>
       <div
-        className={`group select-none transition-all h-64 w-full p-6 rounded-md flex justify-between flex-col ${color} ${color == "bg-black" ? "!text-white border-zinc-500 border-[1px]" : "text-black"}`}
+        className={`group select-none transition-all h-64 w-full p-6 rounded-md flex justify-between flex-col ${isBlackMode ? "" : color} ${
+          isBlackMode
+            ? "!text-white border-zinc-500 border-[1px]"
+            : "text-black"
+        }`}
       >
         <div className="space-y-3 relative">
           <div>
-          <p className={`${color == "bg-black" ? "text-white" : "text-slate-800"} font-semibold w-fit text-sm pb-3 hover:underline`}>@dhvanitmonpara</p>
-            <span onClick={(e)=>{openModal(e)}}
+            <p
+              className={`${
+                isBlackMode ? "text-white" : "text-slate-800"
+              } font-semibold w-fit text-sm pb-3 hover:underline`}
+            >
+              @dhvanitmonpara
+            </p>
+            <span
+              onClick={(e) => {
+                openModal(e);
+              }}
               className="group-hover:opacity-100 transition-all ease-in-out duration-300 absolute right-3 opacity-0 active:scale-95 hover:bg-[#00000015] text-lg cursor-pointer p-3 rounded-full"
             >
               <MdModeEdit />
             </span>
-            <h1 className="text-2xl py-1 font-mono font-bold hover:underline">{title}</h1>
+            <h1 className="text-2xl py-1 font-mono font-bold hover:underline">
+              {title}
+            </h1>
           </div>
           <p className="text-sm font-semibold">{description}</p>
         </div>
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 overflow-x-scroll">
           <AvatarGroup width="w-7" height="h-7" imgSrcArray={imgArray} />
+          {isBlackMode ? (
+            <div className={`w-7 h-7 ${color} rounded-full`}></div>
+          ) : (
+            ""
+          )}
           {/* TODO: map tags */}
-          <Tag isBlackEnable={color == "bg-black" ? true : false} text={tagname} />
-          <Tag isBlackEnable={color == "bg-black" ? true : false} text={tagname} />
-          <Tag isBlackEnable={color == "bg-black" ? true : false} text={tagname} />
-          <Tag isBlackEnable={color == "bg-black" ? true : false} text={tagname} />
+          <Tag
+            isBlackEnable={isBlackMode}
+            text={tagname}
+          />
+          <Tag
+            isBlackEnable={isBlackMode}
+            text={tagname}
+          />
         </div>
       </div>
     </>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import { DocCard, ListCard, Header } from "../components";
-import { useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import useProfileStore from "@/store/profileStore";
+import toggleThemeModeAtRootElem from "@/utils/toggleThemeMode";
+import { themeType } from "@/lib/types";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
 
 function App() {
   const navigate = useNavigate();
   const modalRef = useRef<HTMLDivElement | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalContent, setModalContent] = useState<string | ReactNode>("");
 
   const closeModal = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (modalRef.current === e.target) {
@@ -16,12 +28,18 @@ function App() {
     }
   };
 
-  // const { profile } = useProfileStore();
   const { profile, changeTheme } = useProfileStore();
-
-  // changeTheme("black")
   const { theme } = profile;
-  const checkThemeStatus = theme == "black" ? "!bg-black !text-while" : ""
+  const checkThemeStatus = theme == "black" ? "!bg-black !text-while" : "";
+
+  const themeHandler = (theme: themeType) => {
+    changeTheme(theme);
+    if (theme == "black") {
+      toggleThemeModeAtRootElem("dark");
+    } else {
+      toggleThemeModeAtRootElem(theme);
+    }
+  };
 
   document.addEventListener("keydown", ({ key }) => {
     if (key == "Escape" && isModalOpen) {
@@ -54,56 +72,61 @@ function App() {
 
   return (
     <>
-      <div className={`grid grid-cols-7 dark:bg-zinc-800 black:bg-black ${checkThemeStatus}`}>
-        <div className="col-span-2 py-5 px-7 space-y-3 overflow-y-scroll no-scrollbar max-h-screen">
-          <div className="border-2 dark:border-gray-400 rounded flex justify-center items-center">
+      <div
+        className={`grid grid-cols-7 dark:bg-zinc-800 black:bg-black ${checkThemeStatus}`}
+      >
+        <div className="col-span-2 relative py-5 px-7 space-y-3 overflow-y-scroll no-scrollbar max-h-screen">
+          <div className="border-2 top-0 sticky h-12 dark:bg-zinc-800 z-20 dark:border-gray-600 rounded flex justify-center items-center">
             <span className="font-mono select-none font-black text-2xl dark:text-white">
               Linkaroo
             </span>
-            <button className="text-white" onClick={()=>{changeTheme("black")}}>
-              change theme
-            </button>
           </div>
           <ListCard
             tagname="Hello"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="Hellow"
-            color={theme == "black" ? "bg-black" : "bg-red-400"}
+            color="bg-red-400"
+            isBlackMode={theme == "black" ? true : false}
             setIsModalOpen={setIsModalOpen}
           />
           <ListCard
             tagname="lol"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="lol"
-            color={theme == "black" ? "bg-black" : "bg-emerald-400"}
+            color="bg-emerald-400"
+            isBlackMode={theme == "black" ? true : false}
             setIsModalOpen={setIsModalOpen}
           />
           <ListCard
             tagname="Hello"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="Hellow"
-            color={theme == "black" ? "bg-black" : "bg-amber-400"}
+            color="bg-amber-400"
+            isBlackMode={theme == "black" ? true : false}
             setIsModalOpen={setIsModalOpen}
           />
           <ListCard
             tagname="Hello"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="Hellow"
-            color={theme == "black" ? "bg-black" : "bg-sky-400"}
+            color="bg-sky-400"
+            isBlackMode={theme == "black" ? true : false}
             setIsModalOpen={setIsModalOpen}
           />
           <ListCard
             tagname="Hello"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="Hellow"
-            color={theme == "black" ? "bg-black" : "bg-purple-400"}
+            isBlackMode={theme == "black" ? true : false}
+            color="bg-purple-400"
             setIsModalOpen={setIsModalOpen}
           />
           <ListCard
             tagname="Hello"
             description="bruhh can you Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa odit"
             title="Hellow"
-            color={theme == "black" ? "bg-black" : "bg-green-400"}
+            isBlackMode={theme == "black" ? true : false}
+            color="bg-green-400"
             setIsModalOpen={setIsModalOpen}
           />
         </div>
@@ -206,52 +229,56 @@ function App() {
               color={theme == "black" ? "bg-black" : "bg-emerald-400"}
               setIsModalOpen={setIsModalOpen}
             />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
-            <DocCard
-              title="kya hua"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
-              color={theme == "black" ? "bg-black" : "bg-emerald-400"}
-              setIsModalOpen={setIsModalOpen}
-            />
           </div>
         </div>
         <div className="col-span-2 px-5 overflow-y-scroll max-h-screen no-scrollbar">
-          <div className="min-h-full w-full border-2"></div>
+          <div className="min-h-full w-full border-2">
+            <div
+              className={`sticky bottom-0 z-20 border-2 dark:text-white dark:bg-zinc-800 bg-white text-black border-zinc-300 dark:border-zinc-700 rounded-md`}
+            >
+              <button
+                className="w-full dark:text-white py-3 px-6 rounded-md focus:outline-none hover:bg-zinc-200 dark:hover:bg-zinc-700"
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setModalContent(
+                    <div className="flex h-full w-full flex-col justify-center p-5 items-center">
+                      <h1 className="dark:text-white text-4xl pb-11">
+                        Settings
+                      </h1>
+                      <div className="flex justify-between w-full">
+                        <span className="dark:text-white">Themes:</span>
+                        <Select
+                          onValueChange={(value: themeType) => {
+                            themeHandler(value);
+                          }}
+                        >
+                          <SelectTrigger className="dark:text-white max-w-36">
+                            <SelectValue placeholder="Change theme" />
+                          </SelectTrigger>
+                          <SelectContent
+                            className={
+                              theme != "light"
+                                ? "!bg-black !text-white border-zinc-800"
+                                : ""
+                            }
+                          >
+                            <SelectGroup>
+                              <SelectLabel>Themes</SelectLabel>
+                              <SelectItem value="light">Light</SelectItem>
+                              <SelectItem value="dark">Dark</SelectItem>
+                              <SelectItem value="black">Black</SelectItem>
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                    </div>
+                  );
+                }}
+              >
+                Dhvanit Monpara
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       {isModalOpen && (
@@ -260,7 +287,9 @@ function App() {
           onClick={(e) => closeModal(e)}
           className="fixed inset-0 bg-black bg-opacity-30 backdrop:blur-sm flex justify-center items-center"
         >
-          <div className="h-3/6 w-5/12 bg-zinc-100 dark:bg-zinc-800 rounded-xl"></div>
+          <div className="h-3/6 w-5/12 bg-zinc-100 dark:bg-zinc-800 rounded-xl">
+            {modalContent}
+          </div>
         </div>
       )}
     </>
