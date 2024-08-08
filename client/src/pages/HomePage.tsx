7b34f624a09faf93413ec4f0af3d32bd71467367
Dhvanit <monparadhvanit@gmail.com>
2024-08-08 22:51:58 +0530

    added icons

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -15,6 +15,10 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import DocScreen from "@/components/DocScreen";
+import { MdHome } from "react-icons/md";
+import { IoListSharp } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import { IoPerson } from "react-icons/io5";
 
 function App() {
   const navigate = useNavigate();
@@ -50,6 +54,15 @@ function App() {
     }
   });
 
+  const switchTab = (id: string) => {
+    const navTabs = document.querySelectorAll(".nav-tab");
+    navTabs?.forEach((tab) => {
+      tab.classList.remove("dark:text-white text-black");
+    });
+
+    document.getElementById(id)?.classList.add("dark:text-white text-black");
+  };
+
   useEffect(() => {
     (async () => {
       try {
@@ -75,9 +88,9 @@ function App() {
   return (
     <>
       <div
-        className={`grid grid-cols-7 dark:bg-zinc-800 black:bg-black ${checkThemeStatus}`}
+        className={`grid xl:grid-cols-7 lg:grid-cols-5 grid-cols-3 dark:bg-zinc-800 black:bg-black ${checkThemeStatus}`}
       >
-        <div className="col-span-2 relative py-5 px-7 space-y-3 no-scrollbar max-h-screen">
+        <div className="col-span-2 lg:inline-block hidden relative py-5 px-7 space-y-3 no-scrollbar max-h-screen">
           <div className="border-2 top-0 h-12 dark:bg-zinc-800 z-20 dark:border-zinc-700 rounded flex justify-center px-7 items-center">
             <span className="font-mono select-none font-black text-2xl dark:text-zinc-300">
               Linkaroo
@@ -134,10 +147,14 @@ function App() {
             />
           </div>
         </div>
-        <div className="col-span-3 max-h-screen">
-          <Header theme={theme} setIsModalOpen={setIsModalOpen} setModalContent={setModalContent} />
+        <div className="col-span-3 xl:px-0 lg:px-0 lg:pr-5 px-5 md:max-h-screen">
+          <Header
+            theme={theme}
+            setIsModalOpen={setIsModalOpen}
+            setModalContent={setModalContent}
+          />
           {/* <img src="" alt="Banner" /> */}
-          <div className="h-[calc(100vh-5rem)] overflow-y-scroll w-full space-y-2 no-scrollbar">
+          <div className="md:h-[calc(100vh-5rem)] h-[calc(100vh-9rem)] overflow-y-scroll w-full space-y-2 no-scrollbar">
             <DocCard
               title="kya hua"
               text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus aliquam corrupti corporis repudiandae magnam sapiente quas saepe vero enim ex, aperiam, quaerat consequuntur quidem! Ullam voluptate nam voluptas error quaerat."
@@ -235,8 +252,40 @@ function App() {
               setIsModalOpen={setIsModalOpen}
             />
           </div>
+          <div className="lg:hidden md:fixed bottom-0 dark:text-zinc-400 justify-center items-center px-14 flex w-full h-16">
+            <div className="flex justify-between dark:bg-zinc-800 h-full md:px-32 md:w-8/12 rounded-t-xl items-center w-full sm:w-5/12">
+              <button
+                id="listsTab"
+                onClick={() => {switchTab("listsTab")}}
+                className="navTabs h-full flex justify-center items-center w-16 cursor-pointer text-xl font-semibold dark:hover:text-white"
+                >
+                <IoListSharp />
+              </button>
+              <button
+                id="homeTab"
+                onClick={() => {switchTab("homeTab")}}
+                className="navTabs h-full flex justify-center items-center w-16 cursor-pointer text-xl font-semibold dark:hover:text-white"
+                >
+                <MdHome />
+              </button>
+              <button
+                id="addTab"
+                onClick={() => {switchTab("addTab")}}
+                className="navTabs h-full flex justify-center items-center w-16 cursor-pointer text-lg font-semibold dark:hover:text-white"
+                >
+                <FaPlus />
+              </button>
+              <button
+                id="profileTab"
+                onClick={() => {switchTab("profileTab")}}
+                className="navTabs h-full flex justify-center items-center w-16 cursor-pointer text-lg font-semibold dark:hover:text-white"
+              >
+                <IoPerson />
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="col-span-2 relative px-5 pt-5 space-y-3 overflow-y-scroll max-h-screen no-scrollbar">
+        <div className="col-span-2 hidden xl:inline-block relative px-5 pt-5 space-y-3 overflow-y-scroll max-h-screen no-scrollbar">
           <div
             className={`sticky top-0 z-20 border-2 dark:text-white dark:bg-zinc-800 bg-white text-black border-zinc-200 dark:border-zinc-700 rounded-md`}
           >
@@ -317,9 +366,9 @@ function App() {
         <div
           ref={modalRef}
           onClick={(e) => closeModal(e)}
-          className="fixed inset-0 bg-black bg-opacity-30 backdrop:blur-sm flex justify-center items-center"
+          className="fixed inset-0 bg-black bg-opacity-30 backdrop:blur-sm flex justify-end items-end md:justify-center md:items-center"
         >
-          <div className="h-3/6 w-5/12 bg-zinc-100 dark:bg-zinc-800 rounded-xl">
+          <div className="xl:h-3/6 xl:w-5/12 lg:w-6/12 md:h-3/6 md:w-8/12 h-4/6 w-screen bg-zinc-100 dark:bg-zinc-800 rounded-xl">
             {modalContent}
           </div>
         </div>
