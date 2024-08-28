fd87e4b527094ab23752f14dd0f8d33c43970ec7
Dhvanit <monparadhvanit@gmail.com>
2024-08-28 15:19:41 +0530

    fixed color tiles issue

--- client/src/components/Forms/EditListForm.tsx
+++ client/src/components/Forms/EditListForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "@/components/ui/button";
-import { Loader2, Check } from "lucide-react"; // Import Check icon
+import { Loader2 } from "lucide-react"; // Import Check icon
 import {
   Select,
   SelectItem,
@@ -19,6 +19,8 @@ import useListStore from "@/store/listStore";
 import { Textarea } from "../ui/textarea";
 import useDocStore from "@/store/docStore";
 import useMethodStore from "@/store/MethodStore";
+import "./style/EditListForm.css";
+import { cn } from "@/lib/utils";
 
 type EditListFormProps = {
   theme: themeType | undefined;
@@ -114,88 +116,92 @@ const EditListForm: React.FC<EditListFormProps> = ({ theme, toggleModal }) => {
   };
 
   return (
-    <div className="dark:text-white p-5 flex flex-col justify-center items-center space-y-3">
-      <h1 className="text-3xl">Edit List</h1>
-      <form
-        className="h-4/5 flex flex-col space-y-6 sm:w-96 w-72 justify-center items-center"
-        onSubmit={handleSubmit(handleListCreation)}
-      >
-        <div className="w-full space-y-2">
-          <label htmlFor="title">Title</label>
-          <Input
-            id="title"
-            type="text"
-            placeholder="Enter title"
-            className="dark:bg-zinc-700 bg-zinc-200"
-            {...register("title", { required: "Title is required" })}
-          />
-        </div>
-        <div className="w-full space-y-2">
-          <label htmlFor="description">Description</label>
-          <Textarea
-            id="description"
-            placeholder="Enter description"
-            className="dark:bg-zinc-700 bg-zinc-200"
-            {...register("description", {
-              required: "Description is required",
-            })}
-          />
-        </div>
-        <div className="w-full space-y-2">
-          <span>Theme</span>
-          <Controller
-            name="theme"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Select value={value} onValueChange={onChange}>
-                <SelectTrigger className="dark:text-white dark:bg-zinc-700 max-w-96">
-                  <SelectValue placeholder="Change theme" />
-                </SelectTrigger>
-                <SelectContent
-                  className={
-                    theme !== "light"
-                      ? "!bg-zinc-900 !text-white border-zinc-800"
-                      : ""
-                  }
-                >
-                  <SelectGroup>
-                    <div className="grid grid-cols-4 gap-1 p-2">
-                      {themeOptionsArray.map((themeOption) => (
-                        <SelectItem
-                          key={themeOption.value}
-                          value={themeOption.value}
-                          className={`${themeOption.value} text-zinc-900 p-4 text-center rounded border border-transparent hover:border-zinc-500 dark:hover:border-zinc-400 active:border-zinc-800 focus:outline-none`}
-                        >
-                          {/* Hidden checkmark indicator */}
-                          <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center invisible">
-                            <Check className="h-4 w-4" />
-                          </span>
-                          {themeOption.label}
-                        </SelectItem>
-                      ))}
-                    </div>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            )}
-          />
-        </div>
+    <>
+      <div className="dark:text-white p-5 flex flex-col justify-center items-center space-y-3">
+        <h1 className="text-3xl">Edit List</h1>
+        <form
+          className="h-4/5 flex flex-col space-y-6 sm:w-96 w-72 justify-center items-center"
+          onSubmit={handleSubmit(handleListCreation)}
+        >
+          <div className="w-full space-y-2">
+            <label htmlFor="title">Title</label>
+            <Input
+              id="title"
+              type="text"
+              placeholder="Enter title"
+              className="dark:bg-zinc-700 bg-zinc-200"
+              {...register("title", { required: "Title is required" })}
+            />
+          </div>
+          <div className="w-full space-y-2">
+            <label htmlFor="description">Description</label>
+            <Textarea
+              id="description"
+              placeholder="Enter description"
+              className="dark:bg-zinc-700 bg-zinc-200"
+              {...register("description", {
+                required: "Description is required",
+              })}
+            />
+          </div>
+          <div className="w-full space-y-2">
+            <span>Theme</span>
+            <Controller
+              name="theme"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Select value={value} onValueChange={onChange}>
+                  <SelectTrigger className="dark:text-white dark:bg-zinc-700 max-w-96">
+                    <SelectValue placeholder="Change theme" />
+                  </SelectTrigger>
+                  <SelectContent
+                    className={cn(
+                      theme !== "light"
+                        ? "!bg-zinc-900 !text-white border-zinc-800"
+                        : "",
+                      "[&_[role=option]]:p-0" // Remove padding from all options
+                    )}
+                  >
+                    <SelectGroup>
+                      <div className="grid grid-cols-4 gap-1 p-2">
+                        {themeOptionsArray.map((themeOption) => (
+                          <SelectItem
+                            key={themeOption.value}
+                            value={themeOption.value}
+                            className={cn(
+                              themeOption.value,
+                              "text-transparent p-4 h-16 text-center flex justify-center items-center rounded border border-none hover:text-black dark:hover:text-black active:text-black focus:outline-none",
+                              "data-[state=checked]:font-semibold data-[state=checked]:!text-zinc-900",
+                              "[&>span>span]:hidden"
+                            )}
+                          >
+                            {themeOption.label}
+                          </SelectItem>
+                        ))}
+                      </div>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          </div>
 
-        {loading ? (
-          <Button
-            disabled
-            className="dark:bg-zinc-700 dark:hover:bg-zinc-600 hover:bg-zinc-300 text-zinc-900 bg-zinc-200 w-full dark:text-white cursor-wait"
-          >
-            <Loader2 className="mr-2 h-4 w-4 animate-spin dark:text-white" />
-            Please wait
-          </Button>
-        ) : (
-          <Button className="dark:bg-zinc-700 bg-zinc-200 font-semibold text-zinc-950 dark:text-white hover:bg-zinc-300 dark:hover:bg-zinc-600 w-full">
-            Save changes
-          </Button>
-        )}
-      </form>
-    </div>
+          {loading ? (
+            <Button
+              disabled
+              className="dark:bg-zinc-700 dark:hover:bg-zinc-600 hover:bg-zinc-300 text-zinc-900 bg-zinc-200 w-full dark:text-white cursor-wait"
+            >
+              <Loader2 className="mr-2 h-4 w-4 animate-spin dark:text-white" />
+              Please wait
+            </Button>
+          ) : (
+            <Button className="dark:bg-zinc-700 bg-zinc-200 font-semibold text-zinc-950 dark:text-white hover:bg-zinc-300 dark:hover:bg-zinc-600 w-full">
+              Save changes
+            </Button>
+          )}
+        </form>
+      </div>
+    </>
   );
 };
 
