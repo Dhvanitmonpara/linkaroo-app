2c95e3977c3bea36f9c3a81c8dacc88f999118ff
Dhvanit <monparadhvanit@gmail.com>
2024-09-02 18:18:03 +0530

    fixed header for small screens

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -13,6 +13,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { IoSearchSharp } from "react-icons/io5";
 import { CreateDocForm, CreateListForm } from "./Forms";
 import useMethodStore from "@/store/MethodStore";
 import useProfileStore from "@/store/profileStore";
@@ -42,12 +43,20 @@ const Header = () => {
 
   return (
     <>
-      <nav className="lg:h-20 h-20 lg:py-5 pt-4 pb-[1rem] flex justify-between items-center space-x-2 px-4 border-b-[1px] border-zinc-600 lg:px-32 select-none">
-        <div className="text-zinc-300 transition-colors cursor-pointer text-2xl font-helvetica font-semibold hover:text-white flex space-x-2 justify-center items-center">
-          <img src="/linkaroo.png" alt="Linkaroo-logo" width="24px" className="h-[24px]"/>
-          <Link to={"/"}>Linkaroo</Link>
-        </div>
-        <div className="text-zinc-400 flex space-x-7 font-semibold">
+      <nav className="lg:h-20 h-20 lg:py-5 pt-4 pb-[1rem] flex justify-between items-center space-x-2 px-5 lg:px-32 border-b-[1px] border-zinc-600 select-none">
+        <Link
+          to={"/"}
+          className="text-zinc-300 transition-colors cursor-pointer text-2xl font-helvetica font-semibold hover:text-white flex space-x-2 justify-center items-center"
+        >
+          <img
+            src="/linkaroo.png"
+            alt="Linkaroo-logo"
+            width="24px"
+            className="h-[24px]"
+          />
+          <span className="hidden sm:inline">Linkaroo</span>
+        </Link>
+        <div className="text-zinc-400 md:flex hidden space-x-7 font-semibold">
           <span className="hover:text-zinc-100 cursor-pointer transition-colors">
             Home
           </span>
@@ -61,10 +70,10 @@ const Header = () => {
             Docs
           </span>
         </div>
-        <div className="lg:h-20 h-[4.5rem] lg:py-5 pt-4 pb-[1rem] flex space-x-2 w-2/5">
+        <div className="lg:h-20 h-[4.5rem] lg:py-5 pt-4 pb-[1rem] flex space-x-2 md:w-2/5">
           <Input
             onClick={() => setOpen(true)}
-            className="dark:bg-zinc-800 dark:text-white dark:border-zinc-600"
+            className="dark:bg-zinc-800 dark:text-white hidden md:inline-block dark:border-zinc-600"
             placeholder="Create or Search something..."
           />
 
@@ -101,18 +110,24 @@ const Header = () => {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          <div className="w-12 flex justify-center items-center dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:text-white rounded-sm border-2 dark:border-zinc-700 border-zinc-200 hover:bg-zinc-200">
+          <div
+            onClick={() => setOpen(true)}
+            className="md:w-12 w-10 text-xl md:text-base md:hidden flex justify-center items-center md:dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:text-zinc-300 rounded-sm md:border-2 md:dark:border-zinc-700 md:border-zinc-200 hover:bg-zinc-200"
+          >
+            <IoSearchSharp />
+          </div>
+          <div className="md:w-12 w-10 text-xl md:text-base flex justify-center items-center md:dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:text-white rounded-sm md:border-2 md:dark:border-zinc-700 md:border-zinc-200 hover:bg-zinc-200">
             <IoFilterOutline />
           </div>
           <Popover>
-            <PopoverTrigger className="w-12 h-full lg:block hidden">
-              <div className="w-full h-full flex justify-center items-center dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:text-white rounded-sm border-2 dark:border-zinc-700 border-zinc-200 hover:bg-zinc-200">
+            <PopoverTrigger className="md:w-12 w-10 text-xl md:text-base">
+              <div className="w-full h-full flex justify-center items-center text-zinc-300 md:text-zinc-400 md:dark:bg-zinc-800 dark:hover:bg-zinc-600 dark:text-white rounded-sm md:border-2 md:dark:border-zinc-700 md:border-zinc-200 hover:bg-zinc-200">
                 <IoMdNotificationsOutline />
               </div>
             </PopoverTrigger>
             <PopoverContent
               className={`${
-                theme == "dark"
+                theme == "dark" || theme == "black"
                   ? "bg-zinc-900 border-zinc-700 text-zinc-100"
                   : "bg-zinc-200 border-zinc-300 text-zinc-950"
               } space-y-2 overflow-y-scroll h-[32rem]`}
@@ -128,8 +143,8 @@ const Header = () => {
             </PopoverContent>
           </Popover>
         </div>
-        <div>
-          <ProfileCard/>
+        <div className="hidden lg:inline-block">
+          <ProfileCard />
         </div>
       </nav>
       <CommandMenu open={open} setOpen={setOpen} />
