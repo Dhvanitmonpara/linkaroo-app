038737317fbbfa09092030c4d1f7505d7f06b4ca
Dhvanit <monparadhvanit@gmail.com>
2024-09-12 15:34:44 +0530

    updated ui

--- client/src/components/ActionButtons/AddCollaborator.tsx
+++ client/src/components/ActionButtons/AddCollaborator.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { IoMdPersonAdd } from "react-icons/io";
+import { IoMdPersonAdd, IoIosCopy } from "react-icons/io";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { handleAxiosError } from "@/utils/handlerAxiosError";
 import { useNavigate } from "react-router-dom";
@@ -18,23 +18,25 @@ import { Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+// import { FaUserCircle } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+// import { FaGoogle } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
 import toast from "react-hot-toast";
-import { IoIosCopy } from "react-icons/io";
 
 const AddCollaborator: React.FC = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
   const [userList, setUserList] = useState<Collaborator[]>([]);
-  const [addUserByEmail, setAddUserByEmail] = useState(false);
-  const [addUserByUsername, setAddUserByUsername] = useState(false);
+  const [searchByEmail, setSearchByEmail] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(-1); // To track which item is focused
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const { profile } = useProfileStore();
   const theme = profile.theme;
   const navigate = useNavigate();
-  const resultRefs = useRef<(HTMLDivElement | null)[]>([]); // Refs for result items
-  const inputRef = useRef<HTMLInputElement>(null); // Ref for input
+  const resultRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const searchUsers = useCallback(
     debounce(async (user: string) => {
@@ -46,16 +48,7 @@ const AddCollaborator: React.FC = () => {
 
       setLoading(true);
       try {
-        const apiEndpoint = addUserByEmail
-          ? "email"
-          : addUserByUsername
-          ? "username"
-          : null;
-
-        if (!apiEndpoint) {
-          toast.error("Please select a search option");
-          return;
-        }
+        const apiEndpoint = searchByEmail ? "email" : "username";
 
         const response: AxiosResponse = await axios.patch(
           `${import.meta.env.VITE_SERVER_API_URL}/users/search/${apiEndpoint}`,
@@ -64,7 +57,7 @@ const AddCollaborator: React.FC = () => {
         );
 
         setUserList(response.data.data);
-        setSelectedIndex(-1); // Reset selected index
+        setSelectedIndex(-1);
       } catch (error) {
         handleAxiosError(error as AxiosError, navigate);
         setUserList([]);
@@ -72,7 +65,7 @@ const AddCollaborator: React.FC = () => {
         setLoading(false);
       }
     }, 500),
-    [navigate]
+    [navigate, searchByEmail]
   );
 
   useEffect(() => {
@@ -85,19 +78,31 @@ const AddCollaborator: React.FC = () => {
     setOpen(false);
   };
 
+  const handleSearchModeToggle = (isEmailSearch: boolean) => {
+    setSearchByEmail(isEmailSearch);
+    setOpen(true);
+    setValue("");
+  };
+
+  const getSharableLink = () => {
+    if (!window.location.href.includes("lists")) {
+      toast.error("Please select a list");
+      return;
+    }
+    let link = window.location.href;
+    if (link.includes("docs")) link = link.split("doc")[0];
+    navigator.clipboard.writeText(link);
+    setOpen(false);
+    toast(
+      <div className="flex space-x-2 justify-center items-center">
+        <IoIosCopy />
+        <span>Link copied to clipboard!</span>
+      </div>
+    );
+  };
+
   return (
-    <DropdownMenu
-      open={open}
-      onOpenChange={() => {
-        setOpen(!open);
-        setValue("");
-        if (!open) {
-          if (addUserByEmail) setAddUserByEmail(false);
-          if (addUserByUsername) setAddUserByUsername(false);
-        }
-      }}
-      modal={false} // Keeps dropdown within the context of the parent
-    >
+    <DropdownMenu open={open} onOpenChange={() => setOpen(!open)} modal={false}>
       <DropdownMenuTrigger asChild>
         <div className="h-full w-full flex justify-center items-center cursor-pointer">
           <IoMdPersonAdd />
@@ -109,85 +114,34 @@ const AddCollaborator: React.FC = () => {
           theme !== "light" && "bg-zinc-900 text-zinc-200 border-zinc-700"
         )}
       >
-        {!addUserByUsername && !addUserByEmail && (
-          <div className="w-full h-full p-2 space-y-2">
-            <Button
-              onClick={() => {
-                setAddUserByUsername(!addUserByUsername);
-              }}
-              className={`w-full flex justify-start items-center ${
-                theme !== "light"
-                  ? "text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
-                  : "bg-zinc-100 hover:bg-zinc-200 text-zinc-950"
-              }`}
-            >
-              Add by username
-            </Button>
-            <Button
-              onClick={() => {
-                setAddUserByEmail(!addUserByEmail);
-              }}
-              className={`w-full flex justify-start items-center ${
-                theme !== "light"
-                  ? "text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
-                  : "bg-zinc-100 hover:bg-zinc-200 text-zinc-950"
-              }`}
-            >
-              Add by email
-            </Button>
-            <Button
-              onClick={() => {
-                if(!window.location.href.includes("lists")){
-                  toast.error("Please select a list");
-                  return
-                }
-                let link = window.location.href;
-                if(link.includes("docs")){
-                  link = link.split("doc")[0]
-                }
-                navigator.clipboard.writeText(link);
-                setOpen(false);
-                toast(
-                  <div className="flex space-x-2 justify-center items-center">
-                    <IoIosCopy />
-                    <span>Link copied to clipboard!</span>
-                  </div>
-                );
-              }}
-              className={`w-full flex justify-start items-center ${
-                theme !== "light"
-                  ? "text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
-                  : "bg-zinc-100 hover:bg-zinc-200 text-zinc-950"
-              }`}
-            >
-              Get a Sharable link
-            </Button>
-          </div>
-        )}
-        {(addUserByEmail || addUserByUsername) && (
-          <>
-            <div
+        <div className="flex justify-center items-center p-2 space-x-2">
+          <div className={cn(theme !== "light" && "bg-zinc-900 text-zinc-200")}>
+            <Input
+              ref={inputRef}
+              placeholder={`Search by ${
+                searchByEmail ? "email" : "username"
+              }...`}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
               className={cn(
-                "p-2",
-                theme !== "light" && "bg-zinc-900 text-zinc-200"
+                "w-full p-2 border rounded",
+                theme !== "light" && "bg-zinc-800 text-zinc-200 border-zinc-700"
               )}
-            >
-              <Input
-                type="text"
-                ref={inputRef}
-                placeholder="Search for a user..."
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                // onKeyDown={() => {
-                //   inputRef.blur()
-                // }}
-                className={cn(
-                  "w-full p-2 border rounded",
-                  theme !== "light" &&
-                    "bg-zinc-800 text-zinc-200 border-zinc-700"
-                )}
-              />
-            </div>
+            />
+          </div>
+          <Button
+            onClick={() => handleSearchModeToggle(!searchByEmail)}
+            className={`w-10 flex justify-center p-0 items-center ${
+              theme !== "light"
+                ? "text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
+                : "bg-zinc-100 hover:bg-zinc-200 text-zinc-950"
+            }`}
+          >
+            {searchByEmail ? <MdAlternateEmail /> : <IoMail />}
+          </Button>
+        </div>
+        {value ? (
+          <>
             <DropdownMenuSeparator />
             <ScrollArea>
               <div className="p-2">
@@ -196,7 +150,7 @@ const AddCollaborator: React.FC = () => {
                     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                     Loading...
                   </DropdownMenuItem>
-                ) : userList.length === 0 && value ? ( // Check if userList is empty and input has value
+                ) : userList.length === 0 ? (
                   <DropdownMenuItem
                     className={cn(
                       "p-2 text-center",
@@ -210,27 +164,39 @@ const AddCollaborator: React.FC = () => {
                     <DropdownMenuItem
                       key={user.email}
                       ref={(el) => (resultRefs.current[index] = el)}
-                      tabIndex={-1} // Make it focusable
                       className={cn(
-                        "flex items-center p-2 cursor-pointer outline-none rounded-sm",
+                        "flex items-center p-2 cursor-pointer rounded-sm",
                         selectedIndex === index
                           ? "bg-zinc-100 text-black"
                           : theme !== "light"
-                          ? "!bg-zinc-900 text-zinc-200 hover:bg-zinc-800"
+                          ? "bg-zinc-900 text-zinc-200 hover:bg-zinc-800"
                           : "bg-white text-black hover:bg-gray-100"
                       )}
                       onClick={() => handleSelect(user.email)}
-                      onKeyDown={(e) =>
-                        e.key === "Enter" && handleSelect(user.email)
-                      }
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") handleSelect(user.email);
+                      }}
                     >
-                      {addUserByEmail ? user.email : `@${user.username}`}
+                      {searchByEmail ? user.email : `@${user.username}`}
                     </DropdownMenuItem>
                   ))
                 )}
               </div>
             </ScrollArea>
           </>
+        ) : (
+          <div className="w-full h-full p-2 space-y-2">
+            <Button
+              onClick={getSharableLink}
+              className={`w-full flex justify-center p-0 items-center ${
+                theme !== "light"
+                  ? "text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
+                  : "bg-zinc-100 hover:bg-zinc-200 text-zinc-950"
+              }`}
+            >
+              Get a sharable link
+            </Button>
+          </div>
         )}
       </DropdownMenuContent>
     </DropdownMenu>
