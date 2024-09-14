915b9030a99ff85f2f5b8670f8f066faf8417c37
Dhvanit <monparadhvanit@gmail.com>
2024-09-14 12:42:52 +0530

    added create document api

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,16 +1,52 @@
 import { Lists } from "@/components";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import useListStore from "@/store/listStore";
 import useProfileStore from "@/store/profileStore"
+import { handleAxiosError } from "@/utils/handlerAxiosError";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
 
     const { theme, fullName } = useProfileStore().profile
+    const { inbox } = useListStore()
+    const navigate = useNavigate()
     const [input, setInput] = useState("")
 
-    const quickAddHandler = async (data: { url: string; title?: string }) => {
-        console.log(data);
+    const [loading, setLoading] = useState(false)
+
+    const quickAddHandler = async ({ url }: { url: string; }) => {
+        try {
+            setLoading(true);
+
+            // const metaData = await axios.get("https://www.amazon.in/")
+            // console.log(metaData);
+
+            const response = await axios.post(
+                `${import.meta.env.VITE_SERVER_API_URL}/cards/${inbox?._id}`,
+                {
+                    title: url,
+                    link: url,
+                },
+                { withCredentials: true }
+            );
+
+            if (response.status !== 201) {
+                toast.error("Failed to create card");
+            }
+
+            toast.success("Doc created successfully")
+
+        } catch (error) {
+            handleAxiosError(error as AxiosError, navigate);
+        } finally {
+            setLoading(false);
+            setInput("")
+        }
     }
 
     return (
@@ -23,8 +59,10 @@ const HomePage = () => {
                 <form onSubmit={(e) => {
                     e.preventDefault();
                     quickAddHandler({ url: input });
-                }} className="mt-8 max-w-5xl w-2/5 relative">
+                }} className="mt-8 max-w-5xl w-2/5 relative"
+                >
                     <Input
+                        disabled={loading}
                         type="text"
                         value={input}
                         onChange={(e) => setInput(e.target.value)}
@@ -33,19 +71,12 @@ const HomePage = () => {
                             ? "bg-zinc-800 text-zinc-100 placeholder-zinc-400"
                             : "bg-white text-zinc-900 placeholder-zinc-500"
                             } rounded-3xl w-full`}
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                                e.preventDefault();
-                                quickAddHandler({ url: input });
-                                setInput('');
-                            }
-                        }}
                     />
                     <Button
                         type="submit"
                         className={`${theme !== "light" ? "bg-zinc-700 hover:bg-zinc-600 text-zinc-200" : "bg-zinc-300 hover:bg-zinc-400"} rounded-full px-8 py-5 absolute top-1 right-1 transition-all ease-in-out ${input !== "" ? "scale-100" : "scale-0"}`}
                     >
-                        Add
+                        {loading ? (<Loader2 className="animate-spin" />) : "Add"}
                     </Button>
                 </form>
             </div>
